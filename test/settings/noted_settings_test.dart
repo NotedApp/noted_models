70d5a98df0589ac66031cb3cdcc2127202530b68
Jaewon Yang <jaewonmichael@gmail.com>
2023-08-18 16:54:49 -0400

    #12: build and fix tests

--- test/settings/noted_settings_test.dart
+++ test/settings/noted_settings_test.dart
@@ -4,18 +4,13 @@ import 'package:test/test.dart';
 void main() {
   group('NotedSettings', () {
     test('creates a default set of settings', () {
-      NotedSettings settings = NotedSettings(
-        style: NotedStyleSettings(),
-      );
+      NotedSettings settings = NotedSettings();
 
       expect(settings.style, NotedStyleSettings());
     });
 
     test('parses to and from json', () {
-      NotedSettings settings = NotedSettings(
-        style: NotedStyleSettings(),
-      );
-
+      NotedSettings settings = NotedSettings();
       String json = settings.toJson();
       NotedSettings parsed = NotedSettings.fromJson(json);
 
@@ -23,10 +18,7 @@ void main() {
     });
 
     test('parses to and from map', () {
-      NotedSettings settings = NotedSettings(
-        style: NotedStyleSettings(),
-      );
-
+      NotedSettings settings = NotedSettings();
       Map<String, dynamic> map = settings.toMap();
       NotedSettings parsed = NotedSettings.fromMap(map);
 
